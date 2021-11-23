3a625d391a9bc132bd51e4ef89ba8ed4ee2d7ef3
Thomas Wallis <thomas.wallis@tu-darmstadt.de>
2021-11-23 17:12:10 +0100

    Remove redundant source

--- photodisk_backup.py
+++ photodisk_backup.py
@@ -10,24 +10,17 @@ python photodisk_backup.py
 Tom Wallis wrote it 4.06.2017
 """
 
-import funs, os
+import os
+
+import funs
 
 # list of possible sources and destinations:
-source_list = [os.path.expanduser('~/Dropbox/Keepass'),
-               '/Volumes/primary_backup']
-dest_list = ['/Volumes/backup_1',
-             '/Volumes/backup_2']
+source_list = ["/Volumes/primary_backup"]
+dest_list = ["/Volumes/backup_1", "/Volumes/backup_2"]
 
 # get valid locations:
-valid_sources = funs.valid_locations(source_list, 'source')
-valid_dests = funs.valid_locations(dest_list, 'dest')
+valid_sources = funs.valid_locations(source_list, "source")
+valid_dests = funs.valid_locations(dest_list, "dest")
 
 # loop over valid sources and destinations
 funs.backup_loop(valid_sources, valid_dests)
-
-
-
-
-
-
-
